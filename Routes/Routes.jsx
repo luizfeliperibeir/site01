057f642fbf50007809e103b7e3c08511a35fe7f7
luizmrri <74222433+luizmrri@users.noreply.github.com>
2021-04-05 21:33:33 -0300

    commit on master

---
+++ Routes/Routes.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Route, Switch } from 'react-router';
+
+const Routes = () => {
+    return (
+        <div>
+        
+        </div>
+    )
+}
+
+export default Routes
