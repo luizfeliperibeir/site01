b760718736b214505297d070a94a71e61161ffcc
Victor Nascimento <hugobn.victor@gmail.com>
2021-04-15 10:42:08 -0300

    add google fonts e arrumando estilizacao

---
+++ src/Components/CustomCard/CustomCard.jsx
@@ -0,0 +1,21 @@
+import { Box, Card, CardContent, CardHeader, CardMedia, Typography } from '@material-ui/core';
+
+import React from 'react';
+
+const CustomCard = ({ icone, imagem, titulo, descricao }) => {
+	return (
+		<Card c>
+			<CardMedia></CardMedia>
+			<CardHeader>
+				<Box></Box>
+			</CardHeader>
+			<CardContent>
+				<span class="material-icons">{icone}</span>
+				<Typography variant="h4">{titulo}</Typography>
+				<Typography variant="h5">{descricao}</Typography>
+			</CardContent>
+		</Card>
+	);
+};
+
+export default CustomCard;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,7 @@
 import { Box, Typography, makeStyles } from '@material-ui/core';
 
+import AcUnitIcon from '@material-ui/icons/AcUnit';
+import CustomCard from '../../Components/CustomCard/CustomCard';
 import CustomHeader from '../../Components/CustomHeader/CustomHeader';
 import React from 'react';
 import lzrLogo from '../../assets/logo.png';
@@ -11,27 +13,13 @@ const useStyles = makeStyles((theme) => ({
 		display: 'flex',
 		flexDirection: 'column',
 	},
-	section1Container: {
+	sectionContainer: {
 		display: 'flex',
-		flexDirection: 'column',
 		height: '900px',
-		justifyContent: 'center',
-		alignItems: 'center',
-		width: '100%',
-		backgroundImage: `url(${'https://i1.wp.com/www.csscodelab.com/wp-content/uploads/2019/11/animated-html-css-ripples-background-example.png?fit=966%2C634&ssl=1'})`,
 		backgroundPosition: 'center',
 		backgroundAttachment: 'fixed',
 		backgroundRepeat: 'no-repeat',
 		backgroundSize: 'cover',
-	},
-	section2Container: {
-		display: 'flex',
-		flexDirection: 'column',
-		height: '900px',
-		justifyContent: 'center',
-		alignItems: 'center',
-		backgroundColor: '#f0f0f0',
-		width: '100%',
 	},
 	tituloStyle: {
 		color: '#ab003c',
@@ -40,59 +28,44 @@ const useStyles = makeStyles((theme) => ({
 	tituloStyle02: {
 		color: 'black',
 		fontFamily: 'Arial',
-		fontSize:'30px',
-		textAlign:'center',
+		fontSize: '30px',
+		textAlign: 'center',
+		width: '40%',
+		margin: '20px 0',
 	},
 	tituloStyle03: {
 		color: 'blue',
 		fontWeight: 'bold',
 		borderBottom: '2px solid silver',
 	},
-	paragrafoStyle01:{
+	paragrafoStyle01: {
 		fontSize: '25px',
-		textAlign:'center',
-		marginTop:'10px',
+		textAlign: 'center',
+		marginTop: '10px',
 	},
-	section3Container:{
-		paddingRight: '20px',
-		display: 'flex',
-		height: '900px',
-		justifyContent: 'center',
-		alignItems: 'center',
-		width: '100%',
-		backgroundImage: `url(${'https://i1.wp.com/www.csscodelab.com/wp-content/uploads/2019/11/animated-html-css-ripples-background-example.png?fit=966%2C634&ssl=1'})`,
-		backgroundPosition: 'center',
-		backgroundAttachment: 'fixed',
-		backgroundRepeat: 'no-repeat',
-		backgroundSize: 'cover',
-	},
-	menuItem:{
+	menuItem: {
 		fontSize: '16px',
 		margin: '0px 3px',
 		padding: '5px',
 		color: 'black',
 	},
-	section4Container:{
-		display: 'flex',
-		flexDirection: 'column',
-		height: '900px',
-		justifyContent: 'center',
-		alignItems: 'center',
-		backgroundImage: `url(${'https://png.pngtree.com/thumb_back/fw800/back_our/20190619/ourmid/pngtree-spring-sports-event-poster-background-material-image_138484.jpg'})`,
-		backgroundPosition: 'center',
-		backgroundAttachment: 'fixed',
-		backgroundRepeat: 'no-repeat',
-		backgroundSize: 'cover',
-		width: '100%',
-	},
 }));
 
 const Home = () => {
 	const classes = useStyles();
+	const url1 =
+		'https://i1.wp.com/www.csscodelab.com/wp-content/uploads/2019/11/animated-html-css-ripples-background-example.png?fit=966%2C634&ssl=1';
 	return (
 		<Box className={classes.root}>
 			<CustomHeader />
-			<Box className={classes.section1Container}>
+			<Box
+				className={classes.sectionContainer}
+				style={{
+					backgroundImage: `url(${url1})`,
+					alignItems: 'center',
+					justifyContent: 'center',
+				}}
+			>
 				<Box className={classes.logoContainer}>
 					<img alt="LZR Logo" src={lzrLogo} style={{ height: '100px' }} />
 				</Box>
@@ -100,42 +73,61 @@ const Home = () => {
 					A SUA HISTÓRIA COMEÇA AQUI
 				</Typography>
 			</Box>
-			<Box className={classes.section2Container}>
-			<Typography variant="h3" className={classes.tituloStyle03}>
-					LZR FITNESS 
+			<Box
+				className={classes.sectionContainer}
+				style={{
+					backgroundColor: '#f0f0f0',
+					alignItems: 'center',
+					justifyContent: 'center',
+					display: 'flex',
+					flexDirection: 'column',
+				}}
+			>
+				<Typography variant="h3" className={classes.tituloStyle03}>
+					LZR FITNESS
 				</Typography>
 				<Typography variant="h3" className={classes.tituloStyle03}>
 					QUEM SOMOS ?
 				</Typography>
 				<Typography variant="h3" className={classes.tituloStyle02}>
-					A Lzr é uma empresa focada na ajuda do seu bem-estar, Para que você se sinta mais completo e saudável no seu dia a dia.
-					Criada em julho de 2020, em meio a pandemia do coronavírus, ainda não funcionamos na modalidade presencial. Com o intuito
-					de ajudar muitas pessoas que passam por um momento complicado. 
+					A Lzr é uma empresa focada na ajuda do seu bem-estar, Para que você se sinta mais completo e
+					saudável no seu dia a dia.
+				</Typography>
+				<Typography variant="h3" className={classes.tituloStyle02}>
+					Criada em julho de 2020, em meio a pandemia do coronavírus, ainda não funcionamos na modalidade
+					presencial. Com o intuito de ajudar muitas pessoas que passam por um momento complicado.
 				</Typography>
 			</Box>
-			<Box className={classes.section3Container}>
-				<Box className={classes.logoContainer}>
+			<Box
+				className={classes.sectionContainer}
+				style={{
+					alignItems: 'center',
+					justifyContent: 'center',
+					backgroundImage: `url(${'https://i1.wp.com/www.csscodelab.com/wp-content/uploads/2019/11/animated-html-css-ripples-background-example.png?fit=966%2C634&ssl=1'})`,
+				}}
+			>
+				{/* 	<Box className={classes.logoContainer}>
 					<img alt="LZR Logo" src={lzrLogo} style={{ height: '100px' }} />
+				</Box> */}
+				<Box>
+					<CustomCard titulo="FOCO" icone={'face'} descricao="nos sos masasdhujahd asdj" />
 				</Box>
-				<Typography className={classes.menuItem} >
-					
-				</Typography>
-				<Typography className={classes.menuItem} >
-					FOCO
-				</Typography>
-				<Typography className={classes.menuItem} >
-					METAS
-				</Typography>
-				<Typography className={classes.menuItem} >
-					OBJETIVOS
-				</Typography>
-				<Typography className={classes.menuItem} >
-					VALORES
-				</Typography>
+				<Box>
+					<CustomCard titulo="METAS" icone={'face'} />
 				</Box>
-				<Box className={classes.section4Container}>
+				<Box>
+					<CustomCard titulo="OBJETIVOS" icone={'face'} />
+				</Box>
+
+				{/* <Typography className={classes.menuItem}></Typography>
+				<Typography className={classes.menuItem}>FOCO</Typography>
+				<Typography className={classes.menuItem}>METAS</Typography>
+				<Typography className={classes.menuItem}>OBJETIVOS</Typography>
+				<Typography className={classes.menuItem}>VALORES</Typography> */}
+			</Box>
+			<Box className={classes.section4Container}>
 				<Typography variant="h3" className={classes.tituloStyle03}>
-						Blog
+					Blog
 				</Typography>
 			</Box>
 		</Box>
